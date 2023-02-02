25a4397aeac35489949228262f1f670135954322
avst96 <avst96@gmail.com>
2023-02-01 22:02:54 -0600

    In progress, needs review

---
+++ specs.sql
@@ -0,0 +1,11 @@
+/* 
+Hi I am a gabbai in a busy shul in an up and coming neighborhood. Currently I am focusing in expanding our shachris minyanim. Currently we only have 4 minyanim at 6, 7, 8, and 9, 
+however the crowd has grown a lot and there is demand for more minyanim. The rav told me that I can't start a minyan before I am sure I can keep it going, however when I try to 
+approach people about committing to a minyan, people find it too hard. Instead I would like to create a database consisting of the people in shul at any given time in the morning and make
+my minyanim off that.
+
+I want to record the name of every person, telephone #, time he walked in to shul, time he started to daven, time he left shul (this info will be kept confidential).
+I also need the date, day of week, and a column showing if he is a regular in the shul. A regular means that he is in shul more then %60 of davening days. 
+
+--reports
+*/
